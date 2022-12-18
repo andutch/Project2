5bfa8294c5907a44b7f1374ff3838dabf652309d
Artyom <artyom.dutchak@gmail.com>
2022-12-17 23:05:51 -0700

    started working on completed tasks

--- Proj2_Angular/Project2v0/src/app/completed-tasks/completed-tasks.component.ts
+++ Proj2_Angular/Project2v0/src/app/completed-tasks/completed-tasks.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { TaskDataXferService } from '../services/task-data-xfer.service';
+import { TASKS, COMPLETEDTASKS } from '../models/example-tasks';
+
 @Component({
   selector: 'app-completed-tasks',
   templateUrl: './completed-tasks.component.html',
   styleUrls: ['./completed-tasks.component.css']
 })
 export class CompletedTasksComponent {
+  constructor(public taskDataXfer: TaskDataXferService, private router: Router){
 
+    this.populateExampleTasks()
+  };
+
+
+
+  // populateExampleTasks():void {
+  //   for (let task in TASKS)
+  //     {this.taskDataXfer.taskList.push(TASKS[task]);
+  //     console.log(this.taskDataXfer.taskList)
+  //       }
+
+
+
+  taskList = this.taskDataXfer.completedTaskList;
+
+
+  modifyTask(taskId: string): void {
+    console.log("passed variable: " + taskId);
+    this.taskDataXfer.searchId = taskId;
+    this.taskDataXfer.search();
+    this.router.navigateByUrl('/modify-tasks');
+  }
+
+
+  sortArr(){this.taskDataXfer.sortTasks()}
+
+/////////////////
+    populateExampleTasks():void {
+    for (let task in COMPLETEDTASKS)
+      {
+        // this.taskDataXfer.taskList.sort((a:any, b:any) => a.dueDate - b.dueDate).push(TASKS[task]);
+        this.taskDataXfer.taskList.push(COMPLETEDTASKS[task]);
+
+      console.log(this.taskDataXfer.taskList)
+
+      this.taskDataXfer.sortTasks();
+        }}
+       
 }
--- Proj2_Angular/Project2v0/src/app/models/example-tasks.ts
+++ Proj2_Angular/Project2v0/src/app/models/example-tasks.ts
@@ -56,3 +56,59 @@ taskId: '4567'
 
 
 ]
+
+export const COMPLETEDTASKS:Task[]=[
+
+    {
+    title: 'Finish Project 2',
+    dueDate: '2022-12-04',
+    content:' “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum hic repellat repellendus! Quos voluptatum neque atque, dolor, facilis cupiditate adipisci fugiat praesentium hic molestiae dicta odio nemo. Ducimus, voluptates sed!  ”',
+    priority: 2,
+    timestamp: 'Due: 04-Dec-2204-Dec-22',
+    currentDate: new Date(),
+    taskId: '4567'
+    
+    },
+    {
+    title: ' Hit the gym work out. New years resolution.',
+    dueDate: '2022-12-06',
+    content:' “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum hic repellat repellendus!   ”',
+    priority: 0,
+    timestamp: 'Due: 04-Dec-22',
+    currentDate: new Date(),
+    taskId: '4567'
+    
+    },
+    {
+    title: 'Study for java cert',
+    dueDate: '2022-12-08',
+    content:' “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum hic repellat repellendus! Quos voluptatum neque atque, dolor, facilis cupiditate adipisci fugiat praesentium hic molestiae dicta odio nemo. Ducimus, voluptates sed!  ”',
+    priority: 0,
+    timestamp: 'Due: 04-Dec-2204-Dec-22',
+    currentDate: new Date(),
+    taskId: '4567'
+    
+    },
+    {
+    title: 'Take a dog out for a walk. Go to the park.',
+    dueDate: '2022-12-07',
+    content:' “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum hic repellat repellendus! Quos voluptatum neque atque, dolor, facilis cupiditate adipisci fugiat praesentium hic molestiae dicta odio nemo. Ducimus, voluptates sed!  ”',
+    priority: 2,
+    timestamp: 'Due: 04-Dec-2204-Dec-22',
+    currentDate: new Date(),
+    taskId: '4567'
+    
+    },
+    {
+    title: 'Shopping list for groceries and other stuff',
+    dueDate: '2022-12-01',
+    content:' “ Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum hic repellat repellendus! Quos voluptatum neque atque, dolor, facilis cupiditate adipisci fugiat praesentium hic molestiae dicta odio nemo. Ducimus, voluptates sed!  ”',
+    priority: 0,
+    timestamp: 'Due: 04-Dec-2204-Dec-22',
+    currentDate: new Date(),
+    taskId: '4567'
+    
+    }
+    
+    
+    ]
